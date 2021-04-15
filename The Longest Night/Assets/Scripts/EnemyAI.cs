4b8cc107305d8c48a2da4711b849185cd7dbdee2
Mirza-Krnjic <krnjicmirza@gmail.com>
2021-04-15 13:31:51 +0200

    basic prototyping

---
+++ The Longest Night/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.AI; //this one u have to include
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)// if enemy sees d player
+        {
+            isProvoked = true;
+
+        }
+    }
+
+    void EngageTarget() // chase and attack
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+    void AttackTarget()
+    {
+        Debug.Log(name + " is attacking " + target.name);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
