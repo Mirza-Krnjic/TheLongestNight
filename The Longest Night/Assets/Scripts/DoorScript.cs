f6370e3f080a9ae98033684881b8a2232ca228bc
Mirza-Krnjic <krnjicmirza@gmail.com>
2021-05-09 11:36:49 +0200

    added tree, water collider, finisher the door script

--- The Longest Night/Assets/Scripts/DoorScript.cs
+++ The Longest Night/Assets/Scripts/DoorScript.cs
@@ -8,42 +8,60 @@ public class DoorScript : MonoBehaviour
     private Animator anim;
 
     public bool locked;
-    public string doorType;
+    private string doorType;
 
     //UI
     public bool isOpen = false;
     private bool inRange = false;
     [SerializeField] Text doorOpenText;
     [SerializeField] Text doorCloseText;
+    [SerializeField] Text needDoorKeyText;
 
     //sound related vars
     private AudioSource myPlayer;
     [SerializeField] AudioClip CabinDoorSound;
     [SerializeField] AudioClip RoomDoorSound;
     [SerializeField] AudioClip HouseDoorSound;
+    //door types
     [SerializeField] bool cabin;
     [SerializeField] bool room;
     [SerializeField] bool house;
+    [SerializeField] bool church;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        doorOpenText.gameObject.SetActive(false);
-        doorCloseText.gameObject.SetActive(false);
+        setDoorText();
         myPlayer = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        HasKey4Door();
 
         if (Input.GetKeyDown(KeyCode.E) && inRange)
         {
-            DoorOpen();
-            PlayDoorSound();
+            if (!locked)
+            {
+                DoorOpen();
+                PlayDoorSound();
+            }
         }
 
+    }
 
+    void HasKey4Door()
+    {
+        if (room == true && SaveScript.RoomKey == true)
+            locked = false;
+        if (cabin == true && SaveScript.CabinKey == true)
+            locked = false;
+        if (house == true && SaveScript.HouseKey == true)
+            locked = false;
+        if (church == true && SaveScript.CurchKey == true)
+            locked = false;
     }
 
     public void DoorOpen()
@@ -64,21 +82,30 @@ public class DoorScript : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         inRange = true;
-        if (isOpen)
+
+        if (locked)
+        {
+            needDoorKeyText.text = "You need the " + doorType + " key.";
+            needDoorKeyText.gameObject.SetActive(true);
+        }
+        else if (isOpen)
         {
             doorOpenText.gameObject.SetActive(false);
             doorCloseText.gameObject.SetActive(true);
+            needDoorKeyText.gameObject.SetActive(false);
         }
         else
         {
             doorOpenText.gameObject.SetActive(true);
             doorCloseText.gameObject.SetActive(false);
+            needDoorKeyText.gameObject.SetActive(false);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         doorOpenText.gameObject.SetActive(false);
         doorCloseText.gameObject.SetActive(false);
+        needDoorKeyText.gameObject.SetActive(false);
 
         inRange = false;
     }
@@ -100,5 +127,20 @@ public class DoorScript : MonoBehaviour
             myPlayer.clip = RoomDoorSound;
             myPlayer.Play();
         }
+    }
+    void setDoorText()
+    {
+        doorOpenText.gameObject.SetActive(false);
+        doorCloseText.gameObject.SetActive(false);
+        needDoorKeyText.gameObject.SetActive(false);
+
+        if (cabin == true)
+            doorType = "cabin";
+        else if (room == true)
+            doorType = "room";
+        else if (house == true)
+            doorType = "house";
+        else if (church == true)
+            doorType = "church";
     }
 }
